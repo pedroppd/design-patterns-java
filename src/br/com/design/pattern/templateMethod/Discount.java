b609fb6ac2fbcde7f961a0f2ae841ff23061992f
pedroppd <pedro_pdantas@hotmail.com>
2022-02-19 13:25:57 -0300

    Template method

---
+++ src/br/com/design/pattern/templateMethod/Discount.java
@@ -0,0 +1,27 @@
+package br.com.design.pattern.templateMethod;
+
+import br.com.design.pattern.strategy.Orcamento;
+
+import java.math.BigDecimal;
+
+public abstract class Discount {
+
+    public Discount next;
+
+    public Discount(Discount next){
+        this.next = next;
+    }
+
+    public BigDecimal calculator(Orcamento orcamento) {
+        if(shouldApply(orcamento)) {
+            return performCalculation(orcamento);
+        }
+
+        return this.next.calculator(orcamento);
+    }
+
+    public abstract BigDecimal performCalculation(Orcamento orcamento);
+
+    public abstract boolean shouldApply(Orcamento orcamento);
+
+}
---
+++ src/br/com/design/pattern/templateMethod/DiscountToBudgetWithValueGreatestThenFiveHundred.java
@@ -0,0 +1,30 @@
+package br.com.design.pattern.templateMethod;
+
+import br.com.design.pattern.strategy.Orcamento;
+
+import java.math.BigDecimal;
+
+public class DiscountToBudgetWithValueGreatestThenFiveHundred extends Discount {
+
+    public DiscountToBudgetWithValueGreatestThenFiveHundred(Discount discount){
+        super(discount);
+    }
+
+    public BigDecimal calculator(Orcamento orcamento) {
+        if(shouldApply(orcamento)) {
+            return performCalculation(orcamento);
+        }
+        return this.next.calculator(orcamento);
+    }
+
+    @Override
+    public BigDecimal performCalculation(Orcamento orcamento) {
+        return orcamento.getValue().multiply(new BigDecimal("0.1"));
+    }
+
+    @Override
+    public boolean shouldApply(Orcamento orcamento) {
+        return orcamento.getValue().compareTo(new BigDecimal("500")) > 0;
+    }
+
+}
